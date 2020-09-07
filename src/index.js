8d7bbafb49fbccc5c9c9a0f97c3c8dea60466a0e
Markus Leppänen <46569902+marklepp@users.noreply.github.com>
2020-09-08 01:11:02 +0300

    Click for "x" or "o"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 (function () {
+  var nextMark = (function () {
+    var mark = "o";
+    return function nextMark() {
+      if (mark === "o") {
+        mark = "x";
+      } else {
+        mark = "o";
+      }
+      return mark;
+    };
+  })();
+
+  function initCell() {
+    var cell = document.createElement("td");
+    cell.addEventListener("click", function markCell(e) {
+      e.target.innerHTML = nextMark();
+      e.target.removeEventListener("click", markCell);
+    });
+    return cell;
+  }
+
   function initRow() {
     var row = document.createElement("tr");
-    row.append(
-      document.createElement("td"),
-      document.createElement("td"),
-      document.createElement("td"),
-      document.createElement("td"),
-      document.createElement("td")
-    );
+    row.append(initCell(), initCell(), initCell(), initCell(), initCell());
     return row;
   }
+
   function initializeBoard() {
     var board = document.getElementById("board");
     board.append(initRow(), initRow(), initRow(), initRow(), initRow());
